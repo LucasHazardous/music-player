057ae6f3dbe21b9753242d4c2fca6fe64d540b21
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2022-11-28 08:29:55 +0100

    electron node file with file reading

---
+++ main.js
@@ -0,0 +1,86 @@
+const {
+    app,
+    BrowserWindow,
+    Menu
+} = require('electron')
+const path = require('path')
+const {
+    ipcMain
+} = require('electron')
+const os = require("os")
+const fs = require("fs")
+
+const targetPath = path.join(os.homedir(), "music-player")
+const availableFiles = []
+
+const isDev = process.env.NODE_ENV !== "production"
+
+let mainWindow;
+
+const createWindow = () => {
+    mainWindow = new BrowserWindow({
+        title: "Image Resizer",
+        width: isDev ? 1000 : 500,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+        },
+    })
+
+    const mainMenu = Menu.buildFromTemplate(menu);
+    Menu.setApplicationMenu(mainMenu);
+
+    if (isDev) mainWindow.webContents.openDevTools()
+
+    mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"))
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+const menu = [{
+    label: "File",
+    submenu: [{
+        label: "Refresh",
+        click: loadFiles
+    }, {
+        label: "Quit",
+        click: app.quit,
+        accelerator: "CmdOrCtrl+W"
+    }]
+}]
+
+function loadFiles() {
+    if (!fs.existsSync(targetPath)) {
+        fs.mkdirSync(targetPath)
+    }
+
+    availableFiles.length = 0
+    fs.readdirSync(targetPath).forEach(file => {
+        availableFiles.push(file)
+    })
+
+    mainWindow.webContents.send("refresh", {
+        "fileList": availableFiles
+    })
+    console.log(availableFiles)
+}
+
+ipcMain.on("readFile", (e, data) => {
+    const buffer = fs.readFileSync(path.join(targetPath, data.file))
+
+    mainWindow.webContents.send("audioData", {
+        "buffer": buffer
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
+
+require('electron-reload')(__dirname)
