8533e4cfc1fe2b164b83d62178aa0ed9b61fdde7
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2022-11-28 09:24:18 +0100

    play, pause and speed up audio

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -1,15 +1,30 @@
 const fileList = document.getElementById("fileList")
+const playButton = document.getElementById("playButton")
+const speedChangeBtn = document.getElementById("speedChangeBtn")
+let fastPlaybackRate = false
+let playing = false
+let audio = new Audio()
 
 ipcRenderer.on("refresh", (data) => {
+    fileList.innerHTML = ""
+
     data.fileList.forEach(file => {
-        const fileElement = document.createElement("div")
-        fileElement.classList.add(...["card-header", "has-text-centered"])
+        const fileElement = document.createElement("a")
+        fileElement.classList.add(...["panel-block", "is-active"])
+        fileElement.innerText = file
+
         fileElement.addEventListener("click", () => {
+            playPauseAction()
             ipcRenderer.send("readFile", {
                 file
             })
+
+            fileList.childNodes.forEach(child => {
+                if (child.innerText == undefined) {} else if (child.innerText != file)
+                    child.classList.remove("has-background-primary-light")
+                else child.classList.add("has-background-primary-light")
+            })
         })
-        fileElement.innerText = file
 
         fileList.appendChild(fileElement)
     })
@@ -20,6 +35,32 @@ ipcRenderer.on("audioData", (data) => {
         type: "audio/webm"
     });
     const url = window.URL.createObjectURL(blob);
-    const audio = new Audio(url)
-    audio.play()
+    audio = new Audio(url)
+    audio.load()
+    if (fastPlaybackRate) audio.playbackRate = 1.5
+    playPauseAction()
 })
+
+speedChangeBtn.addEventListener("click", () => {
+    fastPlaybackRate = !fastPlaybackRate
+    if (fastPlaybackRate) {
+        speedChangeBtn.classList.add("has-background-primary-light")
+        audio.playbackRate = 1.5
+    } else {
+        speedChangeBtn.classList.remove("has-background-primary-light")
+        audio.playbackRate = 1
+    }
+})
+
+playButton.addEventListener("click", playPauseAction)
+
+function playPauseAction() {
+    playing = !playing
+    if (playing) {
+        playButton.innerText = "〡〡"
+        audio.play()
+    } else {
+        playButton.innerText = "▷"
+        audio.pause()
+    }
+}
