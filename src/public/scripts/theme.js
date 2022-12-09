5e5d1303f581a2121edca6f6fd9718a9381afb60
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2022-12-09 19:24:11 +0100

    dark theme: light shadows, dark lines between elements

--- src/public/scripts/theme.js
+++ src/public/scripts/theme.js
@@ -4,8 +4,16 @@ ipcRenderer.on("changeTheme", () => {
 	Storage.darkTheme = !Storage.darkTheme;
 	document.querySelector("html").classList.toggle("dark");
 	document
-		.querySelectorAll("a")
+		.querySelectorAll("a, #downloadHeading")
 		.forEach((paragraph) => paragraph.classList.toggle("dark"));
+
+	document
+		.querySelectorAll("#nav, #controls")
+		.forEach((control) => control.classList.toggle("dark-control-nav"));
+
+	document
+		.querySelector(".card-footer-item:not(:last-child)")
+		.classList.toggle("dark-right-border");
 
 	if (Storage.fileElementList[Storage.currentIndex] != undefined) {
 		Storage.previousSelectedIndex = Storage.currentIndex;
