4d37f50f67d79b974bd91a9d2492393c7fa863dc
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2022-12-18 15:26:03 +0100

    change classes when switching themes

--- src/public/scripts/theme.js
+++ src/public/scripts/theme.js
@@ -11,6 +11,13 @@ ipcRenderer.on("changeTheme", () => {
 	toggleClassForElements("dark-panel", ".panel");
 	toggleClassForElements("dark-downloadField", "#downloadField");
 
+	switchClassesForElements(
+		"dark-background",
+		"has-background-primary-light",
+		"#loopBtn",
+		"#speedChangeBtn"
+	);
+
 	if (Storage.fileElementList[Storage.currentIndex] != undefined) {
 		Storage.previousSelectedIndex = Storage.currentIndex;
 		Storage.lightUpCurrentElement();
@@ -21,4 +28,16 @@ function toggleClassForElements(targetClass, ...elements) {
 	document
 		.querySelectorAll(elements.join(", "))
 		.forEach((element) => element.classList.toggle(targetClass));
+}
+
+function switchClassesForElements(class1, class2, ...elements) {
+	document.querySelectorAll(elements.join(", ")).forEach((element) => {
+		if (element.classList.contains(class1)) {
+			element.classList.remove(class1);
+			element.classList.add(class2);
+		} else if (element.classList.contains(class2)) {
+			element.classList.remove(class2);
+			element.classList.add(class1);
+		}
+	});
 }
