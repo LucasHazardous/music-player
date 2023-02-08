b4adf8157130eaa92de987e7965eb9731f2bc1e8
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-02-08 19:57:17 +0100

    exported blocked related event handling to blocked.js module

--- src/public/scripts/playing.js
+++ src/public/scripts/playing.js
@@ -54,40 +54,6 @@ ipcRenderer.on("refresh", (data) => {
 		Storage.lightUpCurrentElement();
 });
 
-ipcRenderer.on("unblockAll", () => {
-	if (Storage.blocked.size == 0) return;
-
-	Storage.blocked = new Set();
-	Storage.fileElementList.forEach((element) =>
-		element.classList.remove("blocked")
-	);
-});
-
-ipcRenderer.on("blockAll", () => {
-	Storage.blocked = new Set(
-		Storage.fileElementList.map((element) => element.trueName)
-	);
-	Storage.fileElementList.forEach((element) =>
-		element.classList.add("blocked")
-	);
-});
-
-ipcRenderer.on("saveBlocked", () => {
-	ipcRenderer.send("blockedList", {
-		files: Storage.blocked,
-	});
-});
-
-ipcRenderer.on("initialBlockedLoad", (data) => {
-	Storage.blocked = new Set();
-	Storage.fileElementList.forEach((element) => {
-		if (data.blocked.has(element.trueName)) {
-			Storage.blocked.add(element.trueName);
-			element.classList.add("blocked");
-		}
-	});
-});
-
 function playFile(file) {
 	URL.revokeObjectURL(Storage.audioElement.getAttribute("src"));
 	Storage.audioElement.setAttribute("src", "");
